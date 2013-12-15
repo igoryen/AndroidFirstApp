0dbcde9693d7bb58f493fc40cb3a8a13c7e5657b
Yggy <igor.yentaltsev@gmail.com>
2013-12-15 13:17:26 -0500

    Added EditText

--- src/com/example/firstapp/MainActivity.java
+++ src/com/example/firstapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -14,7 +15,7 @@ public class MainActivity extends Activity {
     }
 
     public void calculate(View v){
-    	
+    	EditText num1text=(EditText)findViewById(R.id.num1text);
     }
     
     @Override
